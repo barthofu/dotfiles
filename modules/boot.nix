25155f013d481785591a9d22ade3a97484bf7390
Bartholomé Gili <dev.bartho@gmail.com>
2023-11-07 23:57:10 +0100

    feat(nix/modules): boot luks encryption

--- modules/boot.nix
+++ modules/boot.nix
@@ -6,6 +6,14 @@
     efi.canTouchEfiVariables = true;
   };
 
+  boot.initrd.luks.devices = {
+    crypted = {
+      device = "/dev/disk/by-uuid/8670aa9e-78ab-4416-a736-1ac13e40a6df";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+
   boot.kernelParams = [ "processor.max_cstate=4" "amd_iomu=soft" "idle=nomwait"];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
