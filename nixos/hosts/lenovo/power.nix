2cca6e538463374a12224e946188e84f52ff4447
barthofu <dev.bartho@gmail.com>
2024-10-16 11:57:38 +0000

    feat(nixos/lenovo): power management

---
+++ nixos/hosts/lenovo/power.nix
@@ -0,0 +1,23 @@
+{ config, ... }:
+
+{    
+    services = {
+        power-profiles-daemon.enable = !config.services.tlp.enable;
+        thermald.enable = true;
+        tlp = {
+            enable = true;
+            # settings = {
+            #     # Platform
+            #     PLATFORM_PROFILE_ON_BAT = "low-power";
+            #     PLATFORM_PROFILE_ON_AC = "perfomance";
+                    
+            #     # Processor
+            #     CPU_SCALING_MAX_FREQ_ON_AC = 3200000;
+            #     CPU_BOOST_ON_BAT = 0;
+            #     CPU_BOOST_ON_AC = 1;
+            #     CPU_HWP_DYN_BOOST_ON_BAT = 0;
+            #     CPU_HWP_DYN_BOOST_ON_AC = 1;
+            # };
+        };
+    };
+}
