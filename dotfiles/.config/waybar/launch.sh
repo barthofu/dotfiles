f7fea9c7d2a5dc016ef9f4695ebd02075d0f4f48
Bartholomé Gili <dev.bartho@gmail.com>
2024-10-23 17:48:10 +0200

    update(dots): waybar config

--- dotfiles/.config/waybar/launch.sh
+++ dotfiles/.config/waybar/launch.sh
@@ -15,5 +15,5 @@ sleep 0.5
 # ----------------------------------------------------- 
 # Check if waybar-disabled file exists
 if [ ! -f $HOME/.cache/waybar-disabled ] ;then 
-	waybar -c ~/.config/waybar/configs/$config/config -s ~/.config/waybar/configs/$config/themes/$theme.css &
+	waybar -c ~/.config/waybar/configs/$config/config.jsonc -s ~/.config/waybar/configs/$config/themes/$theme.css &
 fi
